d9067d25decc39b99bdf962c908a8ac517ff172c
asrim10 <asrimmsuwall@gmail.com>
2025-11-08 13:50:41 +0545

    extended_system_transaction_history_monthly_interest

--- lib/bank_account.dart
+++ lib/bank_account.dart
@@ -1,4 +1,6 @@
 //1. Base class
+// ignore_for_file: unnecessary_getters_setters, unnecessary_brace_in_string_interps, prefer_final_fields
+
 abstract class BankAccount {
   final int _accountNumber;
   String _accountHolder;
@@ -35,6 +37,25 @@ abstract class BankAccount {
     print("Account Number : $_accountNumber");
     print("Account Holder : $_accountHolder");
     print("Balance        : $_balance");
+    print("Transaction History:");
+    if (_transactions.isEmpty) {
+      print(" - No transactions yet.");
+    } else {
+      for (var t in _transactions) {
+        print(" - $t");
+      }
+    }
+  }
+
+  //For transaction history
+  List<String> _transactions = [];
+
+  List<String> get transactions {
+    return _transactions;
+  }
+
+  void addTransaction(String message) {
+    _transactions.add(message);
   }
 }
 
@@ -45,16 +66,17 @@ abstract class InterestBearing {
 
 //2. Implement three types of accounts that inherit from BankAccount
 class SavingsAccount extends BankAccount implements InterestBearing {
-  double _minBalance = 500;
-  double _interestRate = 0.02;
+  final double _minBalance = 500;
+  final double _interestRate = 0.02;
   double _withdrawCount = 0;
-  double _withdrawLimit = 3;
+  final double _withdrawLimit = 3;
   SavingsAccount(super._accNumber, super._accHolderName, super._balance);
 
   @override
   double deposit(double amount) {
     if (amount > 0) {
-      _balance = _balance + amount;
+      _balance += amount;
+      addTransaction("Deposited \$${amount}");
       print("Deposited \$${amount}. New Balance: \$${_balance}");
     } else {
       print("Invalid deposit amount!");
@@ -65,63 +87,65 @@ class SavingsAccount extends BankAccount implements InterestBearing {
   @override
   double withdraw(double amount) {
     if (_withdrawCount >= _withdrawLimit) {
-      print("Withdrawal limit reached (3 per month).");
-    } else if (_balance - amount < _minBalance) {
-      print(
-        "Cannot withdraw. Minimum balance of \$$_minBalance must be maintained.",
-      );
+      print("Withdrawal limit reached.");
+    } else if (balance - amount < _minBalance) {
+      print("Cannot withdraw below minimum balance.");
     } else {
-      _balance = _balance - amount;
+      balance -= amount;
       _withdrawCount++;
-      print("Withdrawn \$${amount}. New Balance: \$${_balance}");
+      addTransaction("Withdrew \$${amount}");
+      print("Withdrew \$${amount}. New Balance: \$${balance}");
     }
-    return _balance;
+    return balance;
   }
 
   @override
   void calculateInterest() {
-    double interest = _balance * _interestRate;
-    _balance = _balance + interest;
-    print("Interest of \$${interest} added. New Balance: \$${_balance}");
+    double interest = balance * _interestRate;
+    balance += interest;
+    addTransaction("Interest added: \$${interest}");
+    print("Interest \$${interest} added. New Balance: \$${balance}");
   }
 }
 
 class CheckingAccount extends BankAccount {
-  double _overdraftFee = 500;
+  final double _overdraftFee = 500;
   CheckingAccount(super._accNumber, super._accHolderName, super._balance);
 
   @override
   double deposit(double amount) {
     if (amount > 0) {
-      _balance = _balance + amount;
-      print("Deposited \$${amount}. New Balance: \$${_balance}");
+      balance += amount;
+      addTransaction("Deposited \$${amount}");
+      print("Deposited \$${amount}. New Balance: \$${balance}");
     } else {
-      print("Invalid deposit amount!");
+      print("Invalid deposit amount.");
     }
-    return _balance;
+    return balance;
   }
 
   @override
   double withdraw(double amount) {
     if (amount <= 0) {
       print("Invalid withdrawal amount.");
+    } else if (balance - amount < 0) {
+      balance -= (amount + _overdraftFee);
+      addTransaction(
+        "Overdraft withdrawal: \$${amount} + fee \$${_overdraftFee}",
+      );
+      print("Overdraft! Fee charged. New Balance: \$${balance}");
     } else {
-      _balance = _balance - amount;
-
-      if (_balance < 0) {
-        _balance = _balance - _overdraftFee;
-        print("Overdraft occurred! Fee of \$$_overdraftFee applied.");
-      }
-
-      print("Withdrawn \$${amount}. Current Balance: \$${_balance}");
+      balance -= amount;
+      addTransaction("Withdrew \$${amount}");
+      print("Withdrew \$${amount}. New Balance: \$${balance}");
     }
-    return _balance;
+    return balance;
   }
 }
 
 class PremiumAccount extends BankAccount implements InterestBearing {
-  double _minBalance = 10000;
-  double _interestRate = 0.05;
+  final double _minBalance = 10000;
+  final double _interestRate = 0.05;
 
   PremiumAccount(super._accountNumber, super._accountHolder, super._balance);
 
@@ -129,6 +153,7 @@ class PremiumAccount extends BankAccount implements InterestBearing {
   double deposit(double amount) {
     if (amount > 0) {
       _balance = _balance + amount;
+      addTransaction("Deposited \$${amount}");
       print("Deposited \$${amount}. New Balance: \$${_balance}");
     } else {
       print("Invalid deposit amount!");
@@ -146,6 +171,7 @@ class PremiumAccount extends BankAccount implements InterestBearing {
       );
     } else {
       _balance = _balance - amount;
+      addTransaction("Withdrew \$${amount}");
       print("Withdrawn \$${amount}. Current Balance: \$${_balance}");
     }
     return _balance;
@@ -153,9 +179,10 @@ class PremiumAccount extends BankAccount implements InterestBearing {
 
   @override
   void calculateInterest() {
-    double interest = _balance * _interestRate;
-    _balance = _balance + interest;
-    print("Interest of \$${interest} added. New Balance: \$${_balance}");
+    double interest = balance * _interestRate;
+    balance += interest;
+    addTransaction("Interest added: \$${interest}");
+    print("Interest \$${interest} added. New Balance: \$${balance}");
   }
 }
 
@@ -207,5 +234,48 @@ class Bank {
       _accounts[i].displayAccInfo();
       print("-------------------------------");
     }
+  }
+
+  void applyMonthlyInterest() {
+    print("\nApplying monthly interest to all eligible accounts...");
+    for (int i = 0; i < _accounts.length; i++) {
+      if (_accounts[i] is InterestBearing) {
+        (_accounts[i] as InterestBearing).calculateInterest();
+      }
+    }
+  }
+}
+
+class StudentAccount extends BankAccount {
+  final double _maxBalance = 5000;
+  StudentAccount(super._accountNumber, super._accountHolder, super._balance);
+
+  @override
+  @override
+  double deposit(double amount) {
+    if (balance + amount > _maxBalance) {
+      print("Cannot deposit. Maximum balance of \$$_maxBalance exceeded.");
+    } else if (amount > 0) {
+      balance += amount;
+      addTransaction("Deposited \$${amount}");
+      print("Deposited \$${amount}. New Balance: \$${balance}");
+    } else {
+      print("Invalid deposit amount!");
+    }
+    return balance;
+  }
+
+  @override
+  double withdraw(double amount) {
+    if (amount <= 0) {
+      print("Invalid withdrawal amount.");
+    } else if (amount > balance) {
+      print("Insufficient balance.");
+    } else {
+      balance -= amount;
+      addTransaction("Withdrew \$${amount}");
+      print("Withdrew \$${amount}. New Balance: \$${balance}");
+    }
+    return balance;
   }
 }
--- lib/bank_account_test.dart
+++ lib/bank_account_test.dart
@@ -6,21 +6,27 @@ void main() {
   //Accounts
   SavingsAccount sa1 = SavingsAccount(101, "Ram", 2000);
   CheckingAccount ca1 = CheckingAccount(102, "Shyam", 600);
-  PremiumAccount pa1 = PremiumAccount(103, "Asrim", 999999);
+  PremiumAccount pa1 = PremiumAccount(103, "Asrim", 15000);
+  StudentAccount st1 = StudentAccount(104, "Rohit", 2000);
 
   //Add to Bank
   myBank.addAccount(sa1);
   myBank.addAccount(ca1);
   myBank.addAccount(pa1);
+  myBank.addAccount(st1);
 
   sa1.deposit(200);
   sa1.calculateInterest();
   ca1.withdraw(700);
   pa1.calculateInterest();
+  st1.deposit(4000);
+  st1.deposit(2000);
 
   // Transfer money
   myBank.transfer(101, 103, 1000);
+  myBank.transfer(105, 103, 1000);
 
   // Generate report of all accounts
+  myBank.applyMonthlyInterest();
   myBank.generateReport();
 }
